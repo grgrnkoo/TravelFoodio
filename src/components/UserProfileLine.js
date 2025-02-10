5bccfdb406e329266b8cf94e9bd0f8b7ec3bd019
Oleg <grigorenko996@gmail.com>
2025-02-10 17:10:03 +0700

    popup fixed

--- src/components/UserProfileLine.js
+++ src/components/UserProfileLine.js
@@ -1,34 +1,73 @@
 'use client'
 
 import { Pencil } from 'lucide-react';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import UserPreferenceEdit from './UserPreferenceEdit';
 import { UserContext } from './UserProvider';
-import { updateUserByEmail } from '../../_lib/usersActions';
 
 // One line of data at user profile
 export default function UserProfileLine(props) {
     const { userProfile } = useContext(UserContext);
-    const { userData, styleProp, nameOfLine, editable, oneEditingFieldBoolean, setOneEditingFieldBoolean, id, setIsPopupOpen } = props;
+    const { userData, styleProp, nameOfLine, editable, oneEditingFieldBoolean, setOneEditingFieldBoolean, id, setIsPopupOpen, confirmUpdate, isPopupOpen } = props;
 
     const [hovered, setHovered] = useState(false);
     const [inputState, setInputState] = useState(false);
-    const [optimisticUserData, setOptimisticUserData] = useState(userData);
+    const [optimisticUserData, setOptimisticUserData] = useState(() => userData);
+
+    useEffect(() => {
+        if (!isPopupOpen && inputState) {
+            onXClick();
+        }
+    }, [isPopupOpen]);
+
+    useEffect(() => {
+        setInputState(false);
+        setOptimisticUserData(userData);
+    }, [userData]);
 
     const onXClick = () => {
         setInputState(false);
         setOneEditingFieldBoolean(false);
+        setOptimisticUserData(userData);
     };
 
-    const onCheckClick = (value) => {
-        if (value !== userData) {
-            setOptimisticUserData(value);
+    const onCheckClick = async (value) => {
+        if (value && value !== userData && value !== '') {
             setIsPopupOpen(true);
-            updateUserByEmail(userProfile.email, id, value);
+            const confirmed = await confirmUpdate(id, value); // Wait for confirmation
+            if (confirmed) {
+                setOptimisticUserData(value);
+            } else {
+                onXClick();
+            }
+        } else if (!value) {
+            console.error('No value to patch');
+            throw new Error('No value provided');
         }
         setInputState(false);
         setOneEditingFieldBoolean(false);
     };
+
+    const onPencilClick = () => {
+        if (!oneEditingFieldBoolean) {
+            setInputState(true);
+            setOneEditingFieldBoolean(true);
+        }
+    }
+
+    const handleKeyDown = async (e, value) => {
+
+        console.log(value);
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            e.stopPropagation();
+            await onCheckClick(value);
+        } else if (e.key === 'Escape') {
+            onXClick();
+        }
+        console.log('Key: ');
+        console.log(e.key);
+    }
 
     return (
         <div
@@ -43,17 +82,13 @@ export default function UserProfileLine(props) {
                     inputState={inputState}
                     onXClick={onXClick}
                     onCheckClick={onCheckClick}
+                    handleKeyDown={handleKeyDown}
                 />
             </div>
             {hovered && editable && !inputState && !oneEditingFieldBoolean && (
                 <Pencil
                     className={`h-[1rem] hover:cursor-pointer absolute right-0 rounded-full ${hovered && editable && !inputState ? "block" : "hidden"}`}
-                    onClick={() => {
-                        if (!oneEditingFieldBoolean) {
-                            setInputState(true);
-                            setOneEditingFieldBoolean(true);
-                        }
-                    }}
+                    onClick={onPencilClick}
                 />
             )}
         </div>
