d758047905a43f133cb76c7b30e11569b7e3e2e9
Oleg <grigorenko996@gmail.com>
2025-03-26 17:01:52 +0300

    suspence added

--- src/app/auth/layout.js
+++ src/app/auth/layout.js
@@ -1,5 +1,9 @@
 'use client'
 
 export default function AuthLayout({ children }) {
-  return <>{children}</>
+    return (
+        <div className="w-full min-h-full">
+            {children}
+        </div>
+    )
 }
