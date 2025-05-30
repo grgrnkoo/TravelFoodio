91cfa441d9c61645d943f9cf175a7a1245c4de70
Oleg <grigorenko996@gmail.com>
2025-05-30 12:07:14 +0300

    small adjustments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="container mx-auto px-4">
                 <p className="text-sm mb-2">&copy; {new Date().getFullYear()} FoodSm.art. All rights reserved.</p>
                 <div className="mt-2 flex flex-col sm:flex-row justify-center items-center space-x-4">
-                    <div className="flex justify-center items-center space-x-4">
+                    {/* <div className="flex justify-center items-center space-x-4">
                         <span className="text-gray-500 text-sm">Author</span>
                         <span className="text-gray-500">:</span>
                         <span className="text-gray-500 text-sm">Oleg Grigorenko</span>
@@ -18,7 +18,7 @@ const Footer = () => {
                         <Link href="https://linkedin.com/in/oleg-grigorenko-991664117" className="text-gray-400 hover:text-gray-500 text-sm">LinkedIn</Link>
                         <span className="text-gray-500">:</span>
                         <Link href="https://t.me/grgrnkoo" className="text-gray-400 hover:text-gray-500 text-sm">Telegram</Link>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="w-[300px] h-[1px] bg-slate-200 mx-auto my-2" />
                 <div className="mt-2 flex justify-center items-center space-x-4">
